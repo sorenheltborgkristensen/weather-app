9560defefa367c9c577775266f79786e96f81a98
Søren Heltborg Kristensen <62941288+sorenheltborgkristensen@users.noreply.github.com>
2020-12-17 22:32:38 +0100

    weather app

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.listen(port, () => {
 
 app.use(express.static("public"));
 
+app.get("/location/", async (req, res) => {
+  const api_url = "https://freegeoip.app/json/";
+  const response = await fetch(api_url);
+  const data = await response.json();
+  res.json(data);
+});
+
 app.get("/weather/:lat&:lon", async (req, res) => {
   const api_url =
     "https://api.openweathermap.org/data/2.5/onecall?" +
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,45 +1,36 @@
+async function getLocation() {
+  const locationFetch = await fetch("location/");
+  const locationData = await locationFetch.json();
+  return locationData;
+}
+
 async function getWeather() {
-  // get location
-  const positionFetch = await fetch("https://freegeoip.app/json/");
-  const positionData = await positionFetch.json();
-  const city = positionData.city;
-  const lat = positionData.latitude;
-  const lon = positionData.longitude;
-  // get weather based on location
-  const weatherFetch = await fetch(`weather/${lat}&${lon}`);
+  const location = await getLocation();
+  const city = location.city;
+  const weatherFetch = await fetch(`weather/${location.latitude}&${location.longitude}`);
   const weatherData = await weatherFetch.json();
-  const currentWeather = weatherData.current;
-  const dailyWeather = weatherData.daily;
-  const hourlyWeather = weatherData.hourly;
-  // display weather data
-  displayCurrentWeather(city, currentWeather);
-  displayDailyWeather(dailyWeather);
-  displayHourlyWeather(hourlyWeather);
+  init(city, weatherData);
 }
 
-function displayCurrentWeather(city, currentWeather) {
-  console.log(currentWeather);
-  const sunrise = new Date(currentWeather.sunset * 1000);
-  const sunset = new Date(currentWeather.sunset * 1000);
-  const markupCurrentWeather = `
-    <h2>${city}</h2>
-    <img src="http://openweathermap.org/img/wn/${currentWeather.weather[0].icon}.png" alt="${currentWeather.weather[0].main}"></img>
-    <p>${parseFloat(currentWeather.temp).toFixed(1)}°</p>
-    <p>Følels som: ${parseFloat(currentWeather.feels_like).toFixed(1)}°</p>
-    <p>UV-index: ${currentWeather.uvi}</p>
-    <p>Luftfugtighed: ${currentWeather.humidity}</p>
-    <p>Sol op/ned: ${sunrise.getHours()}:${sunrise.getMinutes()}/${sunset.getHours()}:${sunset.getMinutes()}</p>
-    `;
+function init(city, weatherData) {
+  function current() {
+    const currentWeather = weatherData.current;
+    const sunrise = new Date(currentWeather.sunrise * 1000);
+    const sunset = new Date(currentWeather.sunset * 1000);
+    console.log(sunrise.getHours(), sunset.getHours());
+  }
 
-  document.getElementById("current-weather").innerHTML = markupCurrentWeather;
-}
+  function daily() {
+    console.log(weatherData.daily);
+  }
 
-function displayDailyWeather(dailyWeather) {
-  console.log(dailyWeather);
-}
+  function hourly() {
+    console.log(weatherData.hourly);
+  }
 
-function displayHourlyWeather(hourlyWeather) {
-  console.log(hourlyWeather);
+  current();
+  daily();
+  hourly();
 }
 
 getWeather();
