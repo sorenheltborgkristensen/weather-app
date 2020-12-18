04aef48e839224d1066e5f58b194721f63393ac3
Søren Heltborg Kristensen <62941288+sorenheltborgkristensen@users.noreply.github.com>
2020-12-18 09:44:15 +0100

    weather app

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,26 +6,44 @@ async function getLocation() {
 
 async function getWeather() {
   const location = await getLocation();
-  const city = location.city;
   const weatherFetch = await fetch(`weather/${location.latitude}&${location.longitude}`);
   const weatherData = await weatherFetch.json();
-  init(city, weatherData);
+  init(location, weatherData);
 }
 
-function init(city, weatherData) {
+function init(location, weatherData) {
   function current() {
     const currentWeather = weatherData.current;
+    const city = location.city;
     const sunrise = new Date(currentWeather.sunrise * 1000);
     const sunset = new Date(currentWeather.sunset * 1000);
-    console.log(sunrise.getHours(), sunset.getHours());
+    const currentMarkUp = `
+      <h2>${city}</h2>
+      <div>
+        <img src="http://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png" alt="${currentWeather.weather[0].description}" />
+        <p>${currentWeather.temp}°</p>
+      </div>
+      <ul>
+        <li>Føles som: ${currentWeather.feels_like}°</li>
+        <li>UV index: ${currentWeather.uvi}</li>
+        <li>Luftfugtighed: ${currentWeather.humidity}</li>
+        <li>Sol op/ned: ${sunrise.getHours()}:${sunrise.getMinutes()}/${sunset.getHours()}:${sunset.getMinutes()}</li>
+      </ul>
+    `;
+
+    document.getElementById("current-weather").innerHTML = currentMarkUp;
   }
 
   function daily() {
-    console.log(weatherData.daily);
+    weatherData.daily.map((day) => {
+      console.log(day);
+    });
   }
 
   function hourly() {
-    console.log(weatherData.hourly);
+    weatherData.hourly.map((hour) => {
+      console.log(hour);
+    });
   }
 
   current();
